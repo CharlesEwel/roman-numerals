a9526011c053e35633ad8aaeef688aa65027d93a
Alvin Ng and Charles Ewel <charles@>
2016-06-09 13:19:09 -0700

    add regular expressions to convert roman numerials into next class of letter

--- js/scripts.js
+++ js/scripts.js
@@ -41,34 +41,45 @@ $(document).ready(function() {
 
   var numberToNumeral=function(num){
     var numeralString="";
-    for(i = 1; i<=num/1000; i=1){
-      numeralString=numeralString.concat("M");
-      num-=1000;
-    }
-    for(i = 1; i<=num/500; i=1){
-      numeralString=numeralString.concat("D");
-      num-=500;
-    }
-    for(i = 1; i<=num/100; i=1){
-      numeralString=numeralString.concat("C");
-      num-=100;
-    }
-    for(i = 1; i<=num/50; i=1){
-      numeralString=numeralString.concat("L");
-      num-=50;
-    }
-    for(i = 1; i<=num/10; i=1){
-      numeralString=numeralString.concat("X");
-      num-=10;
-    }
-    for(i = 1; i<=num/5; i=1){
-      numeralString=numeralString.concat("V");
-      num-=5;
-    }
-    for(i = 1; i<=num; i=1){
+    // for(i = 1; i<=num/1000; i=1){
+    //   numeralString=numeralString.concat("M");
+    //   num-=1000;
+    // }
+    // for(i = 1; i<=num/500; i=1){
+    //   numeralString=numeralString.concat("D");
+    //   num-=500;
+    // }
+    // for(i = 1; i<=num/100; i=1){
+    //   numeralString=numeralString.concat("C");
+    //   num-=100;
+    // }
+    // for(i = 1; i<=num/50; i=1){
+    //   numeralString=numeralString.concat("L");
+    //   num-=50;
+    // }
+    // for(i = 1; i<=num/10; i=1){
+    //   numeralString=numeralString.concat("X");
+    //   num-=10;
+    // }
+    // for(i = 1; i<=num/5; i=1){
+    //   numeralString=numeralString.concat("V");
+    //   num-=5;
+    // }
+    for(i = 1; i<=num; i++){
       numeralString=numeralString.concat("I");
-      num-=1;
     }
+    var five = /IIIII/g
+    numeralString=numeralString.replace(five,"V");
+    var ten = /VV/g
+    numeralString=numeralString.replace(ten,"X");
+    var fifty = /XXXXX/g
+    numeralString=numeralString.replace(fifty,"L");
+    var oneHundred = /LL/g
+    numeralString=numeralString.replace(oneHundred,"C");
+    var fiveHundred = /CCCCC/g
+    numeralString=numeralString.replace(fiveHundred,"D");
+    var oneThousand = /DD/g
+    numeralString=numeralString.replace(oneThousand,"M");
     var four = /IIII/
     numeralString=numeralString.replace(four,"IV");
     var forty = /XXXX/
